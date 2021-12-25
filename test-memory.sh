462e5a5ea3f7cbe3f0622c2123da599c23f67179
Chris Castle <github@crc.io>
2021-12-25 15:15:36 -0500

    Get nc command if it doesn't exist in path

--- test-memory.sh
+++ test-memory.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
-# Start a simple HTTP server to run this as a web service
+# Check for nc command. Get it if not found.
+nc_command=$(which nc)
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    curl https://cfhcable.dl.sourceforge.net/project/nc110/community%20releases/nc110.20180111.tar.xz | tar xJ
+    cd nc110
+    make generic
+    nc_command=$(pwd)/nc
+    cd ..
+fi
+
+# Start a simple HTTP server so Render sees a process listening on a port
 while true; do
-    printf 'HTTP/1.1 200 OK\r\n' | nc -l ${PORT} > /dev/null;
+    printf 'HTTP/1.1 200 OK\r\n' | $nc_command -l ${PORT} > /dev/null;
 done &
 
+# Print info about all processes until the node process dies
 no_node=0
 while [ $no_node -lt 2 ]; do
     echo "Processes currently running:"
