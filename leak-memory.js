5a9873657bdddad2ee3701f4511b2ad665f5f1e9
Chris Castle <github@crc.io>
2021-12-27 16:31:09 -0500

    Try different approach to measuring process memory
    Show shared and private memory by process
    Also show more accurate sum of all memory being used by processes

--- leak-memory.js
+++ leak-memory.js
@@ -2,5 +2,4 @@ const leak = [];
 
 setInterval(() => {
     leak.push(new Array(1e6).fill("some string"));
-    console.log(process.memoryUsage());
 }, 1000);
--- test-memory.sh
+++ test-memory.sh
@@ -19,8 +19,20 @@ done &
 # Print info about all processes until the node process dies
 no_node=0
 while [ $no_node -lt 2 ]; do
-    echo "Processes currently running:"
-    ps --no-headers --format "etime pid %cpu %mem rss cmd";
+    echo -e 'PID\tPPID\tRSS\tShared\tPrivate\tOOM Score\tCMD'
+    total=0
+    total_p=0
+    total_s=0
+    while read -r pid ppid args; do
+        if [ -f /proc/$pid/smaps ]; then
+            rss=$(awk 'BEGIN {i=0} /^Rss/ {i = i + $2} END {print i}' /proc/$pid/smaps)
+            pss=$(awk 'BEGIN {i=0} /^Pss/ {i = i + $2} END {print i}' /proc/$pid/smaps)
+            shared=$(awk 'BEGIN {i=0} /^Shared_/ {i = i + $2} END {print i}' /proc/$pid/smaps)
+            private=$(awk 'BEGIN {i=0} /^Private_/ {i = i + $2} END {print i}' /proc/$pid/smaps)
+            oom_score_adj=$(cat /proc/$pid/oom_score_adj)
+            printf '%d\t%d\t%d\t%d\t%d\t%d\t%s\n' "$pid" "$ppid" "$rss" "$shared" "$private" "$oom_score_adj" "$args"
+        fi
+    done < <(ps -o pid,ppid,args)
     sleep 1;
 
     # increment no_node if no node process is running
@@ -29,7 +41,6 @@ while [ $no_node -lt 2 ]; do
     if [ $retVal -ne 0 ]; then
         ((no_node++))
     fi
-
 done &
 
 # this process will get oom killed
